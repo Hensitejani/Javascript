1048b7573013e81c223035b2958725084be54ae7
Hensitejani <168739320+Hensitejani@users.noreply.github.com>
2024-05-02 17:49:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+// console.log('this is my first lec')
+
+// let a, b, c;
+
+// a=5;
+// b=7;
+// c=a+b;
+
+// console.log(a, typeof a);
+// console.log(c, typeof c);
+
+// var a1,b1, c1;
+
+// a1=5;
+// b1=8;
+// a1=b1;
+// b1=a1;
+
+// console.log(a1);
+// console.log(b1);
+
+// let score;
+// score=31;
+// console.log(score);
+
+// if(0){
+//     score = score + 1;
+//     console.log(score);
+// }
+//     else{
+//         score=score-1
+//     console.log(score);
+//     }
+
+const pi=3.14;
+// pi=55
+
+// var fname = Jhon;
+// const lastname = deo;
+// let age = 35;
+//     console.log(age);    
+
+var a, b, c
+    a=9;
+    b=1;
+    b=a;
+    a=b;
+    console.log(b);
+
+   if(true){
+    a=1;
+    console.log(a);
+   }
